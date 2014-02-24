5aba562f5045012533acf5b7a1da5605c1eb1f97
dimamartovoi <dimamartovoi@yandex.ru>
2014-02-25 00:56:39 +0300

    UGP.Core: ParseTree base type members (without 'parse' functionality)

--- UGP.Core/ParseTree.fs
+++ UGP.Core/ParseTree.fs
@@ -1,3 +1,33 @@
 ﻿namespace UGP.Core
 
+type ParseTree(category : Category, parent : ParseTree option, children : ParseTree seq option) =
+    member x.Category with get() = category
+    member x.Parent with get() = parent
+    member x.Children with get() = children
 
+//  Returns true if equals(category) and one-level nearest nodes.
+    override x.Equals(another) =
+        match another with
+            | :? ParseTree as tree -> 
+                                    (x.Category = tree.Category)
+                                    && (match (x.Parent, tree.Parent) with
+                                            | Some(one), Some(another) -> one.Category = another.Category
+                                            | None, None -> true
+                                            | _ -> false)
+                                    && (match (x.Children, tree.Children) with
+                                            | Some(one), Some(another) when (Seq.length one) = (Seq.length another) -> Seq.forall2 ((fun f s -> f.Category = s.Category) : ParseTree -> ParseTree -> bool) one another
+                                            | None, None -> true
+                                            | _ -> false)
+            | _ -> false
+
+//  Returns hash code based on the category and one-level nearest nodes.
+    override x.GetHashCode() =
+        x.Category.GetHashCode() * (match x.Parent with
+                                        | Some(parent) -> parent.Category.GetHashCode()
+                                        | None -> 1)
+                                * (match x.Children with
+                                        | Some(children) -> children |> Seq.fold (fun hash child -> hash * child.Category.GetHashCode()) 1
+                                        | None -> 1)
+
+    new(category, parent) = ParseTree(category, parent, None)
+    new(category) = ParseTree(category, None, None)
